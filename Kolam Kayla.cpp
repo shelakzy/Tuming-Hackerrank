37fb44689fe5c70c22e407b82761360c7ed26847
Shela Rana Kezyah <92391060+shelakzy@users.noreply.github.com>
2021-11-13 12:01:38 +0700

    Add files via upload

---
+++ Kolam Kayla.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+float a;
+float b;
+
+cin>>a;
+b = 3.14*a*a;
+
+printf("%.2f",b);
+    return 0;
+}
+
