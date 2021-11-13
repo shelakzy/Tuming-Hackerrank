089b9df404d05dcf9e1992243b49678aab82e30e
Shela Rana Kezyah <92391060+shelakzy@users.noreply.github.com>
2021-11-13 12:00:13 +0700

    Add files via upload

---
+++ Tugas Matematika.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+int a,b;
+
+    cin>>a; cin>>b;
+   
+    
+    cout<<a + b<<endl;
+    cout<<a - b<<endl;
+    cout<<a * b<<endl;
+    return 0;
+}
